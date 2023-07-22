89f9eb06830ee4cca6ba61772e98604687e3fcc6
Andrew Rowe <roweandrewjohn2@gmail.com>
2023-07-22 16:09:25 -0400

    Change title

--- app.py
+++ app.py
@@ -44,24 +44,19 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     """Log user in"""
-
     # Forget any user_id
     session.clear()
-
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
-
         # Ensure username was submitted
         if not request.form.get("username"):
             return apology("must provide username", 403)
-
         # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
-
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
             return apology("invalid username and/or password", 403)
@@ -80,9 +75,48 @@ def login():
 @app.route("/logout")
 def logout():
     """Log user out"""
-
-    # Forget any user_id
     session.clear()
-
-    # Redirect user to login form
     return redirect("/")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    """Register user"""
+    # User reached route through a form
+    if request.method == "POST":
+        # If username is blank
+        if not request.form.get("username"):
+            return apology("Please Enter a Username and Password")
+        # Save username
+        username = request.form.get("username")
+        # If username already exists
+        rows = db.execute("SELECT * FROM users WHERE username = ?;", username)
+        if len(rows) > 0:
+            return apology("Username already exists")
+
+        # If user didn't input a password
+        if not request.form.get("password") or not request.form.get("confirmation"):
+            return apology("Please enter a password in both password fields")
+        # If passwords do not match
+        if request.form.get("password") != request.form.get("confirmation"):
+            return apology("Passwords do not match. Please reenter password")
+        # Save password
+        password = request.form.get("password")
+
+        # Insert new user into users
+        hash = generate_password_hash(password)
+        db.execute("INSERT INTO users(username, hash) VALUES(?,?);", username, hash)
+
+        users = db.execute("SELECT * FROM users;")
+        print(users)
+
+        # Log the user in
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to homepage
+        return redirect("/")
+
+    # User reached route through clicking a link
+    else:
+        return render_template("register.html")
