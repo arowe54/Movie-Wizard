ddaeff3adc90d409a7b45afc896577f4120f9cd2
Andrew Rowe <roweandrewjohn2@gmail.com>
2023-07-22 15:07:54 -0400

    Add movies.db

--- app.py
+++ app.py
@@ -1,1 +1,41 @@
-print("hello world")
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import date
+
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Custom filter (format values as US dollars)
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use the local filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///movies.db")
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route("/")
+@login_required
+def index():
+    """Home Page"""
+    return apology()
+
