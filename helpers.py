2a05675d69e385a7cd3e11acb5de8a47546a8e8b
Andrew Rowe <roweandrewjohn2@gmail.com>
2023-07-23 22:36:41 -0400

    Add trailer

--- helpers.py
+++ helpers.py
@@ -47,6 +47,24 @@ def all_of_movie(movie_id):
     for key in awards:
         if key not in movie:
             movie[key] = awards[key]
+    
+    # Trailer (videoStrip)
+    querystring = {"info":"videoStrip"}
+    trailer = requests.get(url, headers=headers, params=querystring)
+    trailer = trailer.json()
+    movie["trailer"] = trailer["results"]["videoStrip"]["edges"][0]["node"]
+    
+
+
+    # filmingLocations
+
+    # Soundtrack
+
+    # countriesOfOrigin (maybe add flag icons)
+
+    # Spoken Languages
+
+    # moreLikeThisTitles
 
     return movie
 
