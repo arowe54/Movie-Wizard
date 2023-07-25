d9a9bb4d585964e7131996a523e04be458c3620c
Andrew Rowe <roweandrewjohn2@gmail.com>
2023-07-25 19:15:29 -0400

    Add remove from watchlist

--- scripts/file.js
+++ scripts/file.js
@@ -1,19 +1,47 @@
+var user_id = document.getElementByID('user_id').innerHTML;
+
+
+$(document).ready(function(){
+    // every time the checkbox is clicked
+    $(".form-check-input").click(function() {
+        // Save the movie id
+        var movie_id = $(this).val();
+        
+        // Open database connection
+        const sqlite3 = require('sqlite3').verbose();
+        let db = new sqlite3.Database('./db/movies.db');
+        // Update the database
+        // If the checkbox is already checked (being unchecked)
+        if (this.checked){
+            // Remove movie from watchlist
+            params = [user_id, movie_id]
+            db.run("DELETE FROM watchlist WHERE user_id = ? AND movie_id = ?", params, function(err) {
+                if (err) {
+                    return console.error(err.message);
+                }
+            });
+        }
+        // If it was unchecked (now checked)
+        else {
+            // Add movie to database
+            ids = [user_id, movie_id, user_id];
+            db.run("INSERT INTO watchlist(user_id, movie_id) VALUES (?, ?) WHERE user_id = ?", ids, function(err) {
+                // Print message if couldn't update table
+                if (err) {
+                    return console.error(err.message);
+                }
+            });
+        }
+        // Close database connection
+        db.close();
+    })
+})
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
+/*
 const sqlite3 = require('sqlite3').verbose();
 // Open a database connection
 let db = new sqlite3.Database('./db/movies.db');
@@ -42,12 +70,4 @@ function removeFromWatchlist() {
 
 // Close database connection
 db.close();
-
-// Might need to make the functions update the database and page asynchronously
-
-// use jQuery instead
-
-// select the element
-// change the database when clicked
-
-// Use AJAX
+*/
