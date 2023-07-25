354140f49dd7bd09adea0cc78bd448b6fecbf419
Andrew Rowe <roweandrewjohn2@gmail.com>
2023-07-25 18:11:06 -0400

    Add movie_id to jquery

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -5,36 +5,36 @@ let db = new sqlite3.Database('./db/movies.db');
 const user_id = document.getElementByID('user_id').innerHTML;
 
 
-function addToWatchlist(event){
-    // Create new AJAX object
-    var ajax = new XMLHttpRequest();
 
-    trigger = event.
+// Create new AJAX object
+var ajax = new XMLHttpRequest();
 
-    // When page is loaded
-    ajax.onreadystatechange = function() {
-        if (ajax.readyState == 4 && ajax.status == 200){
-            $(document).ready(function(){
-                // every time the checkbox is clicked
-                $('.form-check-input').click(function(event){
-                    // Save the movie id
-                    // Update the database
-                    // If it is being checked
-                        // Add movie to database
-                        ids = [user_id, movie_id, user_id];
-                        db.run("INSERT INTO watchlist(user_id, movie_id) VALUES (?, ?) WHERE user_id = ?", ids, function(err) {
-                            // Print message if couldn't update table
-                            if (err) {
-                                return console.error(err.message);
-                            }
-                        });
-                    // If it is being unchecked
-                        // Remove movie from database
-                })
+    
+
+// When page is loaded
+ajax.onreadystatechange = function() {
+    if (ajax.readyState == 4 && ajax.status == 200){
+        $(document).ready(function(){
+            // every time the checkbox is clicked
+            $('.form-check-input').click(function(event) {
+                // Save the movie id
+                var movie_id = $(this).val();
+                // Update the database
+                // If the checkbox is being checked
+                    // Add movie to database
+                    ids = [user_id, movie_id, user_id];
+                    db.run("INSERT INTO watchlist(user_id, movie_id) VALUES (?, ?) WHERE user_id = ?", ids, function(err) {
+                        // Print message if couldn't update table
+                        if (err) {
+                            return console.error(err.message);
+                        }
+                    });
+                // If it is being unchecked
+                    // Remove movie from database
             })
-            // Update database
-            
-        }
+        })
+        // Update database
+        
     }
 }
 
