f03bd095fb0ae566137fd0c4d622e415dedf56ef
Andrew Rowe <roweandrewjohn2@gmail.com>
2023-07-26 15:11:08 -0400

    Add keys to movie asynchronously

--- parallel.py
+++ parallel.py
@@ -3,12 +3,19 @@ import aiohttp
 import asyncio
 
 def get_all_info(movie_id):
+    movie = {}
+
     # Get a request from a url with current parameters
     async def fetch(session, url, headers, querystring):
         async with session.get(url, headers=headers, params=querystring) as resp:
             result = await resp.json()
-            return result["results"]
-        
+            result = result["results"]
+            # Copy each key:value pair to the movie dict
+            for key in result:
+                if key not in movie:
+                    movie[key] = result[key]
+
+       
     async def main():
         async with aiohttp.ClientSession() as session:
             url = "https://moviesdatabase.p.rapidapi.com/titles/{}".format(movie_id)
@@ -16,18 +23,14 @@ def get_all_info(movie_id):
                 "X-RapidAPI-Key": "515955a8bbmsh7bacf3e7bb3ed33p1ef576jsna2431a83680e",
                 "X-RapidAPI-Host": "moviesdatabase.p.rapidapi.com"
             }
-            movie = {}
 
             # get request 1
             querystring = {"info":"base_info"}
-            base_info = await fetch(session, url, headers, querystring)
-            print(base_info)
-            print()
+            await fetch(session, url, headers, querystring)
 
             # Get request 2 asynchronously
             q2 = {"info":"revenue_budget"}
-            rev_budget = await fetch(session, url, headers, q2)
-            print(rev_budget)
-            print()
-
+            await fetch(session, url, headers, q2)
+            
     asyncio.run(main())
+    print(movie)
